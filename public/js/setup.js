7eba83c98bff69c1a9bd10f897c400c14258e6b9
Blake Callens <blake@blake-dev.(none)>
2013-11-14 13:52:07 -0500

    removed generatePassword function from public setup.js

--- public/js/setup.js
+++ public/js/setup.js
@@ -39,18 +39,3 @@ function checkPasswordMatch()
         $('#password_check').attr('style', 'color: #00AA00');
     }
 }
-
-function generatePassword()
-{
-    var characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '?'];
-    
-    var password = '';
-    while(password.length < 8)
-    {
-        password = password.concat(characters[parseInt(Math.random() * characters.length)]);
-    }
-    
-    $('#password').val(password);
-    $('#confirm_password').val(password);
-    checkPasswordMatch();
-}
