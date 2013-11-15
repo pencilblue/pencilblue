f823c30709780d1671ea550d07ec5036f67f64af
Blake Callens <blake@blake-dev.(none)>
2013-11-15 15:03:44 -0500

    more work on topics

--- public/js/admin/content/topics/manage_topics.js
+++ public/js/admin/content/topics/manage_topics.js
@@ -3,6 +3,8 @@ function narrowTopics()
     var searchString = $('#topic_search').val().toLowerCase();
     if(searchString.length == 0)
     {
+        $('#topic_search_icon').attr('class', 'glyphicon glyphicon-search');
+    
         $('.topic').each(function()
         {
             $(this).show();
@@ -17,7 +19,7 @@ function narrowTopics()
             var topic = $(this);
             topic.find('.topic_name').each(function()
             {
-                if($(this).html().toLowerCase().indexOf(searchString) == 0)
+                if($(this).html().toLowerCase().indexOf(searchString) > -1)
                 {
                     topic.show();
                 }
@@ -32,8 +34,14 @@ function narrowTopics()
 
 function clearTopicSearch()
 {
-    $('#topic_search_icon').attr('class', 'glyphicon glyphicon-search');
     $('#topic_search').val('');
     $('#topic_search').focus();
     narrowTopics();
+}
+
+function confirmDeleteTopic(siteRoot, topicID, topicName)
+{
+    $('#delete_name').html(topicName);
+    $('#delete_button').attr('onclick', 'window.location = "' + siteRoot + '/actions/admin/content/topics/deleteTopic?id=' + topicID + '"');
+    $('#confirm_delete_modal').modal({backdrop: 'static', keyboard: true});
 }
