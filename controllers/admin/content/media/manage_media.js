641c50960257dbf8ec60a6e1f86d2dd15e1f9f8d
Blake Callens <blake@blake-dev.(none)>
2013-11-21 11:44:23 -0500

    work on manage_media

--- controllers/admin/content/media/manage_media.js
+++ controllers/admin/content/media/manage_media.js
@@ -61,5 +61,90 @@ this.init = function(request, output)
 
 this.getMedia = function(media, output)
 {
-    output('');
+    var mediaList = '';
+    var mediaItemTemplate = ''
+    var instance = this;
+    
+    // Case insensitive sort
+    media.sort(function(a, b)
+    {
+        var x = a['caption'].toLowerCase();
+        var y = b['caption'].toLowerCase();
+    
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+    
+    getHTMLTemplate('admin/content/media/manage_media/media_item', null, null, function(data)
+    {
+        mediaItemTemplate = data;
+    
+        for(var i = 0; i < media.length; i++)
+        {
+            var mediaItemElement = mediaItemTemplate.split('^media_id^').join(media[i]._id.toString());
+            mediaItemElement = mediaItemElement.split('^media_icon^').join(instance.getMediaIcon(media[i].media_type));
+            mediaItemElement = mediaItemElement.split('^media_caption^').join(media[i].caption);
+            mediaItemElement = mediaItemElement.split('^media_thumb^').join(instance.getMediaThumb(media[i].media_type, media[i].location));
+            mediaList = mediaList.concat(mediaItemElement);
+        }
+    
+        output(mediaList);
+    });
+}
+
+this.getMediaIcon = function(mediaType)
+{
+    var iconID = '';
+
+    switch(mediaType)
+    {
+        case 'image':
+            iconID = 'picture-o';
+            break;
+        case 'video/mp4':
+        case 'video/webm':
+        case 'video/ogg':
+            iconID = 'film';
+            break;
+        case 'youtube':
+            iconID = 'youtube';
+            break;
+        case 'vimeo':
+            iconID = 'vimeo-square';
+            break;
+        case 'daily_motion':
+            iconID = 'play-circle-o';
+            break;
+        default:
+            iconID = 'question';
+            break;
+    }
+    
+    return '<i class="fa fa-' + iconID + '"></i>';
+}
+
+this.getMediaThumb = function(mediaType, mediaLocation)
+{
+    switch(mediaType)
+    {
+        case 'image':
+            return '<img src="' + mediaLocation + '" style="max-height: 150px; max-width: 100%"></img>';
+            break;
+        case 'video/mp4':
+        case 'video/webm':
+        case 'video/ogg':
+            return '<video style="max-height: 150px; max-width: 100%"><source src="' + mediaLocation + '" type="' + mediaType + '"></video>'
+            break;
+        case 'youtube':
+            return '<img src="http://img.youtube.com/vi/' + mediaLocation + '/0.jpg" style="max-height: 150px; max-width: 100%"></img>';
+            break;
+        case 'vimeo':
+            return '';
+            break;
+        case 'daily_motion':
+            return '<img src="http://www.dailymotion.com/thumbnail/video/' + mediaLocation + '" style="max-height: 150px; max-width: 100%"></img>';
+            break;
+        default:
+            return '';
+            break;
+    }
 }
