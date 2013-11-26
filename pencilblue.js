2ced10140834f319ae250114293183a9f4d9ccc1
Brian P. Hyder <brianhyder@gmail.com>
2013-11-26 17:46:42 -0500

    EXTERNAL_CONFIGURATION Implementation for creating a configuration outside of the context of the application. This allows developers to have unique configurations for their particular environment. Known Issue: You cannot set the MONGO_SERVER property and have it take effect.  The DB connection is initiated before the configuration is loaded.  This will be corrected in the DAO_WRAPPER branch.

--- pencilblue.js
+++ pencilblue.js
@@ -1,40 +1,68 @@
 // A grouping of all require calls
 require('./include/requirements');
 
-var server = http.createServer(function(request, response)
-{
-    // /include/router.js
-    var route = new Route(request, response);
-    
-    if(request.headers.cookie)
-    {
-        var parsedCookies = {};
-        var cookieParameters = request.headers.cookie.split(';');
-        for(var i = 0; i < cookieParameters.length; i++)
-        {
-            var cookieParameter = cookieParameters[i].split('=');
-            parsedCookies[cookieParameter[0]] = cookieParameter[1];
-        }
-        request.headers['parsed_cookies'] = parsedCookies;
-    }
-    
-    if(request.headers['content-type'])
-    {
-        if(request.headers['content-type'].indexOf('multipart/form-data') > -1)
-        {
-            return;
-        }
-    }
-    
-    request.on('data', function(chunk)
-    {
-        if(typeof request.headers['post'] == 'undefined')
-        {
-            request.headers['post'] = '';
-        }
-        request.headers['post'] += chunk;
-    });
-});
-server.listen(SITE_PORT, SITE_IP);
+/**
+ * To be called when the configuration is loaded.  The function is responsible 
+ * for triggered the startup of the HTTP connection listener as well as start a 
+ * connection pool to the core DB.
+ */
+var onConfigurationLoad = function(){
+	
+	//start core db
+	//TODO fill this in when branch DAO_WRAPPER is merged
+	
+	//start server
+	initServer();
+};
 
-console.log(SITE_NAME + ' running on ' + SITE_ROOT);
+/**
+ * Initializes the server
+ */
+var server;
+function initServer(){
+	console.log('Starting server...');
+	server = http.createServer(function(request, response){
+
+	    // /include/router.js
+	    var route = new Route(request, response);
+	    
+	    if(request.headers.cookie)
+	    {
+	        var parsedCookies = {};
+	        var cookieParameters = request.headers.cookie.split(';');
+	        for(var i = 0; i < cookieParameters.length; i++)
+	        {
+	            var cookieParameter = cookieParameters[i].split('=');
+	            parsedCookies[cookieParameter[0]] = cookieParameter[1];
+	        }
+	        request.headers['parsed_cookies'] = parsedCookies;
+	    }
+	    
+	    if(request.headers['content-type'])
+	    {
+	        if(request.headers['content-type'].indexOf('multipart/form-data') > -1)
+	        {
+	            return;
+	        }
+	    }
+	    
+	    request.on('data', function(chunk)
+	    {
+	        if(typeof request.headers['post'] == 'undefined')
+	        {
+	            request.headers['post'] = '';
+	        }
+	        request.headers['post'] += chunk;
+	    });
+	});
+	
+	server.listen(SITE_PORT, SITE_IP);
+	console.log(SITE_NAME + ' running on ' + SITE_ROOT);
+}
+
+//start up sequence
+//1. Load Requirements
+//2. Load configuration
+//3. Start connection to core DB
+//4. Start HTTP Server
+loadConfiguration(onConfigurationLoad);
