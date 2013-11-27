57ba56e431da9502efbed9355d2c99c25d44073b
Blake Callens <blake@blake-dev.(none)>
2013-11-27 10:12:59 -0500

    fix to fullscreen toggle of content_layout. was being broken by html minification

--- public/js/admin/elements/content_layout.js
+++ public/js/admin/elements/content_layout.js
@@ -1,36 +1,50 @@
 function toggleLayoutFullscreen()
 {
-    if(!$('#layout_editor').attr('style').length)
+    if(!$('#layout_editor').attr('style'))
     {
-        $('#layout_editor').css(
-        {
-            'background-color': '#FFFFFF',
-            'position': 'fixed',
-            'top': '0',
-            'left': '0',
-            'width': '100%',
-            'height': '100%',
-            'overflow': 'auto',
-            'z-index': '50000'
-        });
-        
-        $('#layout_content').css(
-        {
-            'height': ($('#layout_editor').height() - $('#layout_content').position().top) + 'px',
-            'margin': '0'
-        });
-        
-        $('#layout_code').css(
-        {
-            'width': '100%',
-            'height': '100%',
-            'resize': 'none'
-        });
-        $('#layout_code').focus();
-        
-        return;
+        openLayoutFullscreen();
     }
+    else if($('#layout_editor').attr('style').length == 0)
+    {
+        openLayoutFullscreen();
+    }
+    else
+    {
+        closeLayoutFullscreen();
+    }
+}
+
+function openLayoutFullscreen()
+{
+    $('#layout_editor').css(
+    {
+        'background-color': '#FFFFFF',
+        'position': 'fixed',
+        'top': '0',
+        'left': '0',
+        'width': '100%',
+        'height': '100%',
+        'overflow': 'auto',
+        'z-index': '50000'
+    });
     
+    $('#layout_content').css(
+    {
+        'height': ($('#layout_editor').height() - $('#layout_content').position().top) + 'px',
+        'margin': '0'
+    });
+    
+    $('#layout_code').css(
+    {
+        'width': '100%',
+        'height': '100%',
+        'resize': 'none'
+    });
+    $('#layout_code').focus();
+}
+
+function closeLayoutFullscreen()
+{
     $('#layout_editor').attr('style', '');
     $('#layout_content').css(
     {
