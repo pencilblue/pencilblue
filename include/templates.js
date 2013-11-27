a029c0dad35bc488a649356b61608417897e1aa2
Blake Callens <blake@blake-dev.(none)>
2013-11-27 09:52:21 -0500

    added minify extension. js, css, and html is now automatically minified

--- include/templates.js
+++ include/templates.js
@@ -6,47 +6,43 @@ global.getHTMLTemplate = function(templateLocation, pageName, metaDesc, output)
     this.loadTemplate = function()
     {
         // Load the header template HTML and customize
-        fs.readFile(fileLocation, function(error, data)
+        minify.optimize(fileLocation,
         {
-            if(error)
+            callback: function(data)
             {
-                console.log(error.message);
-                output('');
-                return;
+                templateString = data.toString();
+                templateString = templateString.split('^site_name^').join(SITE_NAME);
+                templateString = templateString.split('^site_root^').join(SITE_ROOT);
+                if(typeof pageName !== "undefined")
+                {
+                    templateString = templateString.split('^page_name^').join(' | ' + pageName);
+                }
+                else
+                {
+                    templateString = templateString.split('^page_name^').join('');
+                }
+                
+                if(typeof metaDesc !== "undefined")
+                {
+                    templateString = templateString.split('^meta_desc^').join(metaDesc);
+                }
+                else
+                {
+                    templateString = templateString.split('^meta_desc^').join(SITE_NAME + ' | ' + pageName);
+                }
+                
+                templateString = templateString.split('^year^').join(new Date().getFullYear());
+                
+                var subTemplateCount = templateString.split('^tmp_').length;
+                
+                if(subTemplateCount == 1)
+                {
+                    output(templateString);
+                    return;
+                }
+                
+                instance.loadSubTemplate(templateString, output);
             }
-            
-            templateString = data.toString();
-            templateString = templateString.split('^site_name^').join(SITE_NAME);
-            templateString = templateString.split('^site_root^').join(SITE_ROOT);
-            if(typeof pageName !== "undefined")
-            {
-                templateString = templateString.split('^page_name^').join(' | ' + pageName);
-            }
-            else
-            {
-                templateString = templateString.split('^page_name^').join('');
-            }
-            
-            if(typeof metaDesc !== "undefined")
-            {
-                templateString = templateString.split('^meta_desc^').join(metaDesc);
-            }
-            else
-            {
-                templateString = templateString.split('^meta_desc^').join(SITE_NAME + ' | ' + pageName);
-            }
-            
-            templateString = templateString.split('^year^').join(new Date().getFullYear());
-            
-            var subTemplateCount = templateString.split('^tmp_').length;
-            
-            if(subTemplateCount == 1)
-            {
-                output(templateString);
-                return;
-            }
-            
-            instance.loadSubTemplate(templateString, output);
         });
     }
     
---
+++ public/js/admin/elements/content_layout.js
@@ -0,0 +1,40 @@
+function toggleLayoutFullscreen()
+{
+    if(!$('#layout_editor').attr('style').length)
+    {
+        $('#layout_editor').css(
+        {
+            'background-color': '#FFFFFF',
+            'position': 'fixed',
+            'top': '0',
+            'left': '0',
+            'width': '100%',
+            'height': '100%',
+            'overflow': 'auto',
+            'z-index': '50000'
+        });
+        
+        $('#layout_content').css(
+        {
+            'height': ($('#layout_editor').height() - $('#layout_content').position().top) + 'px',
+            'margin': '0'
+        });
+        
+        $('#layout_code').css(
+        {
+            'width': '100%',
+            'height': '100%',
+            'resize': 'none'
+        });
+        $('#layout_code').focus();
+        
+        return;
+    }
+    
+    $('#layout_editor').attr('style', '');
+    $('#layout_content').css(
+    {
+        'margin-bottom': '1em'
+    });
+    $('#layout_code').attr('style', 'min-height: 200px');
+}
