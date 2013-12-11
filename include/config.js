fe0ed017114654b32295cd01d667cb5129f68e31
Blake Callens <blake.callens@gmail.com>
2013-12-11 10:57:27 -0500

    changes to config to test if config.json exists and to create log file if none exists. left with crash error in dao

--- include/config.js
+++ include/config.js
@@ -12,7 +12,7 @@
 global.DOCUMENT_ROOT = __dirname.substr(0, __dirname.indexOf(path.sep+'include'));
 
 global.LOG_LEVEL = 'debug';
-global.LOG_FILE  = '/etc/pencilblue/log/pencilblue.log';
+global.LOG_FILE  = DOCUMENT_ROOT + '/log/pencilblue.log';
 
 var config = {
 	siteName: 'pencilblue',
@@ -30,8 +30,8 @@ var config = {
 	},
 	logging: {
 		transports: [
-             new (winston.transports.Console)({ level: "debug", timestamp: true }),
-             new (winston.transports.File)({ filename: '/etc/pencilblue/log/pencilblue.log', level: "debug", timestamp: true })
+             new (winston.transports.Console)({ level: LOG_LEVEL, timestamp: true }),
+             new (winston.transports.File)({ filename: LOG_FILE, level: LOG_LEVEL, timestamp: true })
        ]
 	}
 };
@@ -43,27 +43,43 @@ var config = {
  * the system.
  */
 global.loadConfiguration = function() {
-	var result = fs.readFileSync('/etc/pencilblue/config.json', {encoding: "UTF-8"});
-		  
-	var override = null;
-	if (typeof result === 'Error') {
-	    console.log('Failed to read external configuration file. Using defaults: '+err);
-	    return config;
-	}
-	else{
-		try{
-		  override = JSON.parse(result);
-		}
-		catch(e){
-		  console.log('Failed to parse configuration file.  Using defaults: '+e);
-		  return config;
-		}
-	}
+
+    // If no log file exists, we should create one - Blake
+    if(!fs.existsSync(LOG_FILE))
+    {
+        if(!fs.existsSync(DOCUMENT_ROOT + '/log/'))
+        {
+            fs.mkdirSync(DOCUMENT_ROOT + '/log/');
+        }
+        
+        fs.writeFileSync(DOCUMENT_ROOT + '/log/pencilblue.log', '');
+    }
+
+    // Needed to add DOCUMENT_ROOT and a test to make sure the file exists. Was crashing on my setup. - Blake    
+    if(fs.existsSync(DOCUMENT_ROOT + '/config.json'))
+    {
+	    var result = fs.readFileSync(DOCUMENT_ROOT + '/config.json', {encoding: "UTF-8"});
+		      
+	    var override = null;
+	    if (typeof result === 'Error') {
+	        console.log('Failed to read external configuration file. Using defaults: '+err);
+	        return config;
+	    }
+	    else{
+		    try{
+		      override = JSON.parse(result);
+		    }
+		    catch(e){
+		      console.log('Failed to parse configuration file.  Using defaults: '+e);
+		      return config;
+		    }
+	    }
 	
-	for(var key in override) {
-		console.log("Overriding property: KEY="+key+" VAL="+JSON.stringify(override[key]));
-		config[key] = override[key];
-	}
+	    for(var key in override) {
+		    console.log("Overriding property: KEY="+key+" VAL="+JSON.stringify(override[key]));
+		    config[key] = override[key];
+	    }
+    }
 	return config;
 };
 
