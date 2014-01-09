0e97802a959098bf29312af989d06fb133d57c25
Blake Callens <blake.callens@gmail.com>
2014-01-09 14:45:06 -0500

    verification email send and unverified_user created

--- controllers/actions/user/sign_up.js
+++ controllers/actions/user/sign_up.js
@@ -1,6 +1,6 @@
 /*
 
-    Creates an unverified user and sends a confirmation email
+    Creates a user and sends a confirmation email, if necessary
     
     @author Blake Callens <blake.callens@gmail.com>
     @copyright PencilBlue 2013, All rights reserved
@@ -9,6 +9,8 @@
 
 this.init = function(request, output)
 { 
+    var instance = this;
+
     getSession(request, function(session)
     {    
         var post = getPostParameters(request);
@@ -26,7 +28,7 @@ this.init = function(request, output)
         {
             if(!contentSettings.require_verification)
             {
-                var userserDocument = createDocument('user', post);
+                var userDocument = createDocument('user', post);
                 
                 getDBObjectsWithValues({object_type: 'user', username: userDocument['username']}, function(data)
                 {
@@ -63,8 +65,90 @@ this.init = function(request, output)
             }
             else
             {
+                uniqueID(function(verificationCode)
+                {
+                    post['verification_code'] = verificationCode;
                 
+                    var unverifiedUserDocument = createDocument('unverified_user', post);
+                    
+                    getDBObjectsWithValues({object_type: 'user', username: unverifiedUserDocument['username']}, function(data)
+                    {
+                        if(data.length > 0)
+                        {
+                            formError(request, session, '^loc_EXISTING_USERNAME^', '/user/sign_up', output);
+                            return;
+                        }
+                        
+                        getDBObjectsWithValues({object_type: 'user', email: unverifiedUserDocument['email']}, function(data)
+                        {
+                            if(data.length > 0)
+                            {
+                                formError(request, session, '^loc_EXISTING_EMAIL^', '/user/sign_up', output);
+                                return;
+                            }
+                            
+                            getDBObjectsWithValues({object_type: 'unverified_user', username: unverifiedUserDocument['username']}, function(data)
+                            {
+                                if(data.length > 0)
+                                {
+                                    if(data[0].email != unverifiedUserDocument['email'])
+                                    {
+                                        formError(request, session, '^loc_EXISTING_USERNAME^', '/user/sign_up', output);
+                                        return;
+                                    }
+                                }
+                                
+                                getDBObjectsWithValues({object_type: 'unverified_user', email: unverifiedUserDocument['email']}, function(data)
+                                {
+                                    if(data.length > 0)
+                                    {
+                                        editDBObject(data[0]._id, unverifiedUserDocument, [], function(data)
+                                        {
+                                            if(data.length == 0)
+                                            {
+                                                formError(request, session, '^loc_ERROR_SAVING^', '/user/sign_up', output);
+                                                return;
+                                            }
+                                            
+                                            instance.sendVerificationEmail(unverifiedUserDocument);
+                                            
+                                            session.success = '^loc_VERIFICATION_SENT^' + unverifiedUserDocument.email;
+                                            output({redirect: pb.config.siteRoot + '/user/verification_sent'});
+                                        });
+                                        return;
+                                    }
+                                    
+                                    createDBObject(unverifiedUserDocument, function(data)
+                                    {
+                                        if(data.length == 0)
+                                        {
+                                            formError(request, session, '^loc_ERROR_SAVING^', '/user/sign_up', output);
+                                            return;
+                                        }
+                                        
+                                        instance.sendVerificationEmail(unverifiedUserDocument);
+                                            
+                                        session.success = '^loc_VERIFICATION_SENT^' + unverifiedUserDocument.email;
+                                        output({redirect: pb.config.siteRoot + '/user/verification_sent'});
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
             }
         });
+    });
+}
+
+this.sendVerificationEmail = function(user)
+{
+    getEmailSettings(function(emailSettings)
+    {
+        emailBody = emailSettings.verification_content.split('^verification_url^').join(pb.config.siteRoot + '/user/verify_email?code=' + user.verification_code);;
+        emailBody = emailBody.split('^first_name^').join(user.first_name);
+        emailBody = emailBody.split('^last_name^').join(user.last_name);
+        
+        sendEmail(emailSettings.from_name + '<' + emailSettings.from_address + '>', user.email, emailSettings.verification_subject, emailBody);
     });
 }
--- include/email.js
+++ include/email.js
@@ -30,3 +30,56 @@ global.getEmailSettings = function(output)
     
     });
 }
+
+global.sendEmail = function(from, to, subject, body)
+{
+    getEmailSettings(function(emailSettings)
+    {
+        var nodemailer = require("nodemailer");
+        
+        if(emailSettings.service == 'custom')
+        {
+            var smtpTransport = nodemailer.createTransport("SMTP",
+            {
+                host: emailSettings.host,
+                secureConnection: emailSettings.secure_connection,
+                port: emailSettings.port,
+                auth:
+                {
+                    user: emailSettings.username,
+                    pass: emailSettings.password
+                }
+            });
+        }
+        else
+        {
+            var smtpTransport = nodemailer.createTransport("SMTP",
+            {
+                service: emailSettings.service,
+                auth:
+                {
+                    user: emailSettings.username,
+                    pass: emailSettings.password
+                }
+            });
+        }
+        
+        var mailOptions =
+        {
+            from: from,
+            to: to,
+            subject: subject,
+            html: body
+        };
+        
+        smtpTransport.sendMail(mailOptions, function(error, response)
+        {
+            if(error)
+            {
+                console.log(error);
+            }
+
+            smtpTransport.close();
+        });
+    });
+}
