a6e2e223d9b2ec238f5609656d1109877869422d
Brian P. Hyder <brianhyder@gmail.com>
2014-02-12 17:55:32 -0500

    PB-20 (#20) Converted manage_topics

--- controllers/admin/content/topics/manage_topics.js
+++ controllers/admin/content/topics/manage_topics.js
@@ -1,13 +1,67 @@
-/*
+/**
+ * Manage Topics - Interface for managing the site's topics via drag and drop
+ * 
+ * @author Blake Callens <blake@pencilblue.org>
+ * @copyright PencilBlue 2014, All rights reserved
+ */
+function ManageTopics() {}
 
-    Interface for managing the site's topics via drag and drop
+//inheritance
+util.inherits(ManageTopics, pb.BaseController);
+
+ManageTopics.prototype.render = function(cb) {
+	var self = this;
+	var dao  = new pb.DAO();
+	dao.query('topic', pb.DAO.ANYWHERE, pb.DAO.PROJECT_ALL, {name: pb.DAO.ASC}).then(function(topics) {
+		if (util.isError(topics)) {
+			//TODO handle this
+		}
+		
+		//none to manage
+        if(topics.length == 0) {                
+            cb(pb.RequestHandler.generateRedirect(pb.config.siteRoot + '/admin/content/topics/new_topic'));
+            return;
+        }
+        
+        //currently, mongo cannot do case-insensitive sorts.  We do it manually 
+        //until a solution for https://jira.mongodb.org/browse/SERVER-90 is merged.
+        topics.sort(function(a, b) {
+            var x = a['name'].toLowerCase();
+            var y = b['name'].toLowerCase();
+        
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        });
     
-    @author Blake Callens <blake.callens@gmail.com>
-    @copyright PencilBlue 2013, All rights reserved
+        pb.templates.load('admin/content/topics/manage_topics', '^loc_MANAGE_TOPICS^', null, function(data) {
+            var result = ''+data;
+                
+            self.displayErrorOrSuccess(result, function(newSession, newResult) {
+                result = newResult;
+                
+                var pills = require('../topics').getPillNavOptions('manage_topics');
+                pills.unshift(
+                {
+                    name: 'manage_topics',
+                    title: '^loc_MANAGE_TOPICS^',
+                    icon: 'refresh',
+                    href: '/admin/content/topics/manage_topics'
+                });
+                
+                result = result.concat(pb.js.getAngularController(
+                {
+                    navigation: getAdminNavigation(session, ['content', 'topics']),
+                    pills: pills,
+                    topics: topics
+                }, [], 'initTopicsPagination()'));
+                
+                var content = self.localizationService.localize(['admin', 'topics'], result);
+                cb({content: content});
+            });
+        });
+    });
+};
 
-*/
-
-this.init = function(request, output)
+ManageTopics.init = function(request, output)
 {
     var result = '';
     var instance = this;
@@ -74,4 +128,8 @@ this.init = function(request, output)
             });
         });
     });
-}
+};
+
+//exports
+module.exports = ManageTopics;
+
