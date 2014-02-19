6692acec9758a67d4910a7cfd50fc41c487c9304
Blake Callens <blake.callens@gmail.com>
2014-02-19 16:22:32 -0500

    fix to new_object to properly list child_objects that are custom object types

--- controllers/admin/content/custom_objects/new_object.js
+++ controllers/admin/content/custom_objects/new_object.js
@@ -57,7 +57,7 @@ NewObject.prototype.render = function(cb) {
                             name: 'manage_objects',
                             title: '^loc_NEW^ ' + objectType.name,
                             icon: 'chevron-left',
-                            href: '/admin/content/custom_objects/manage_objects/' + objectType.name
+                            href: '/admin/content/custom_objects/manage_objects'
                         },
                         {
                             name: 'new_object',
@@ -115,22 +115,36 @@ NewObject.loadFieldOptions = function(dao, objectType, cb) {
         
         if(objectType.fields[key].object_type.indexOf('custom:') > -1)
         {
-            var customObjectType = objectType.fields[key].object_type.split('custom:').join('');
-            dao.query('custom_object', {type: customObjectType}).then(function(customObjects) {
-		        if (util.isError(customObjects)) {
-			        //TODO handle this
-		        }
-		        
-		        var customObjectsInfo = [];
-		        for(var i = 0; i < customObjects.length; i++)
-		        {
-		            customObjectsInfo.push({_id: customObjects[i]._id, name: customObjects[i].name});
-		        }
-		        
-		        objectType.fields[key].available_objects = customObjectsInfo;
-		        index++;
-		        self.loadObjectOptions(index);
-	        });
+            var customObjectTypeName = objectType.fields[key].object_type.split('custom:').join('');
+            dao.query('custom_object_type', {name: customObjectTypeName}).then(function(customObjectTypes)
+            {
+                if (util.isError(customObjectTypes)) {
+		            //TODO handle this
+	            }
+	            
+	            if(customObjectTypes.length == 0)
+	            {
+	                return;
+	            }
+	            
+	            var customObjectType = customObjectTypes[0];
+            
+                dao.query('custom_object', {type: customObjectType._id.toString()}).then(function(customObjects) {
+		            if (util.isError(customObjects)) {
+			            //TODO handle this
+		            }
+		            
+		            var customObjectsInfo = [];
+		            for(var i = 0; i < customObjects.length; i++)
+		            {
+		                customObjectsInfo.push({_id: customObjects[i]._id, name: customObjects[i].name});
+		            }
+		            
+		            objectType.fields[key].available_objects = customObjectsInfo;
+		            index++;
+		            self.loadObjectOptions(index);
+	            });
+            });
 	        
 	        return;
         }
