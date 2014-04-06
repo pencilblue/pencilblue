9b9351cf61fd378a59fc89e6587c40d90f3d5410
Brian P. Hyder <brianhyder@gmail.com>
2014-04-06 16:45:50 -0400

    #50 Uncommented unit tests

--- test/include/service/entities/plugin_service_test.js
+++ test/include/service/entities/plugin_service_test.js
@@ -40,42 +40,42 @@ module.exports = {
 		pb.utils.onPromisesOk(pb.dbm.shutdown(), cb);
 	},
 	
-//	testValidateValidDetailsJSONFile: function(test) {
-//		
-//		var pluginDirName = 'sample';
-//		var detailsPath   = pb.PluginService.getDetailsPath(pluginDirName);
-//		pb.PluginService.loadDetailsFile(detailsPath, function(err, details) {
-//			test.ok(err == null);
-//			if (err != null) {
-//				pb.log.error("Failed to get details: " + err.stack);
-//			}
-//			
-//			pb.PluginService.validateDetails(details, pluginDirName, function(errs, result) {
-//				
-//				test.ok(result);
-//				test.ok(errs == null);
-//				if (errs) {
-//					pb.log.error(""+errs);
-//					if (util.isArray(errs.validationErrors)) {
-//						for (var i = 0; i < errs.validationErrors.length; i++) {
-//							pb.log.error(""+errs.validationErrors[i]);
-//						}
-//					}
-//				}
-//				test.done();
-//			});
-//		});
-//	},
+	testValidateValidDetailsJSONFile: function(test) {
+		
+		var pluginDirName = 'sample';
+		var detailsPath   = pb.PluginService.getDetailsPath(pluginDirName);
+		pb.PluginService.loadDetailsFile(detailsPath, function(err, details) {
+			test.ok(err == null);
+			if (err != null) {
+				pb.log.error("Failed to get details: " + err.stack);
+			}
+			
+			pb.PluginService.validateDetails(details, pluginDirName, function(errs, result) {
+				
+				test.ok(result);
+				test.ok(errs == null);
+				if (errs) {
+					pb.log.error(""+errs);
+					if (util.isArray(errs.validationErrors)) {
+						for (var i = 0; i < errs.validationErrors.length; i++) {
+							pb.log.error(""+errs.validationErrors[i]);
+						}
+					}
+				}
+				test.done();
+			});
+		});
+	},
 	
-//	testGetServices: function(test) {
-//		
-//		var pathToPlugin = path.join(pb.PluginService.getPluginsDir(), 'sample');
-//		pb.PluginService.getServices(pathToPlugin, function(err, services) {
-//			test.equals(null, err);
-//			test.ok(services.text_creater != null);
-//			test.done();
-//		});
-//	},
+	testGetServices: function(test) {
+		
+		var pathToPlugin = path.join(pb.PluginService.getPluginsDir(), 'sample');
+		pb.PluginService.getServices(pathToPlugin, function(err, services) {
+			test.equals(null, err);
+			test.ok(services.text_creater != null);
+			test.done();
+		});
+	},
 	
 	testInstall: function(test) {
 		
