ba2ee796969dc3b94e54a9e372b68dd15f89e33d
Blake Callens <blake.callens@gmail.com>
2014-05-09 19:04:35 +0000

    removed redundant code from get_media_embed api call

--- controllers/api/content/get_media_embed.js
+++ controllers/api/content/get_media_embed.js
@@ -37,37 +37,9 @@ GetMediaEmbed.prototype.render = function(cb) {
             return;
         }
 
-        cb({content: pb.BaseController.apiResponse(pb.BaseController.API_SUCCESS, mediaObject.name + ' retrieved', self.getEmbedTag(mediaObject))});
+        cb({content: pb.BaseController.apiResponse(pb.BaseController.API_SUCCESS, mediaObject.name + ' retrieved', Media.getMediaEmbed(mediaObject))});
         return;
     });
-};
-
-GetMediaEmbed.prototype.getEmbedTag = function(mediaObject) {
-    switch(mediaObject.media_type) {
-        case 'image':
-            return '<img class="img-responsive" src="' + mediaObject.location + '" style="^media_style^"/>';
-
-        case 'youtube':
-            return '<iframe width="560" height="315" src="//www.youtube.com/embed/' + mediaObject.location + '" frameborder="0" allowfullscreen></iframe>';
-
-        case 'vimeo':
-            return '<iframe src="//player.vimeo.com/video/' + mediaObject.location + '" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-
-        case 'daily_motion':
-            return '<iframe frameborder="0" width="480" height="270" src="http://www.dailymotion.com/embed/video/' + mediaObject.location + '"></iframe>';
-
-        case 'vine':
-            return '<iframe class="vine-embed" src="https://vine.co/v/' + mediaObject.location + '/embed/simple" width="400" height="400"  frameborder="0"></iframe>';
-
-        case 'instagram':
-            return '<iframe src="//instagram.com/p/' + mediaObject.location + '/embed/" width="400" height="475" frameborder="0" scrolling="no" allowtransparency="true"></iframe>';
-        case 'slideshare':
-            return '<iframe src="http://www.slideshare.net/slideshow/embed_code/' + mediaObject.location + '" width="427" height="356" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" allowfullscreen></iframe>';
-        case 'trinket':
-            return '<iframe src="https://trinket.io/embed/python/' + mediaObject.location + '" width="600" height="400" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>';
-    }
-
-    return '';
 };
 
 //exports
