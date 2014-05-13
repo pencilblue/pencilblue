3ffdc808b6747048b6c89d0ded16febfe2e6f2cd
Blake Callens <blake.callens@gmail.com>
2014-05-12 21:48:33 -0400

    fix to edit_object angular script

--- controllers/admin/content/custom_objects/edit_object.js
+++ controllers/admin/content/custom_objects/edit_object.js
@@ -1,6 +1,6 @@
 /**
  * Manage custom objects via a table
- * 
+ *
  * @author Blake Callens <blake@pencilblue.org>
  * @copyright PencilBlue 2014, All rights reserved
  */
@@ -16,33 +16,33 @@ EditObject.prototype.render = function(cb) {
         cb(pb.RequestHandler.generateRedirect(pb.config.siteRoot + '/admin/content/custom_objects/manage_object_types'));
         return;
     }
-	
+
 	var dao  = new pb.DAO();
 	dao.query('custom_object', {_id: ObjectID(vars['id'])}).then(function(customObjects) {
 		if (util.isError(customObjects)) {
 			//TODO handle this
 		}
-		
+
 		//none to manage
-        if(customObjects.length == 0) {                
+        if(customObjects.length == 0) {
             cb(pb.RequestHandler.generateRedirect(pb.config.siteRoot + '/admin/content/custom_objects/manage_object_types'));
             return;
         }
-        
+
         var customObject = customObjects[0];
-        
+
         dao.query('custom_object_type', {_id: ObjectID(customObject.type)}).then(function(customObjectTypes) {
 	        if (util.isError(customObjectTypes)) {
 		        //TODO handle this
 	        }
-	
+
 	        //none to manage
-            if(customObjectTypes.length == 0) {                
+            if(customObjectTypes.length == 0) {
                 cb(pb.RequestHandler.generateRedirect(pb.config.siteRoot + '/admin/content/custom_objects/manage_object_types'));
                 return;
             }
-        
-            EditObject.loadFieldOptions(dao, customObjectTypes[0], function(objectType) {             
+
+            EditObject.loadFieldOptions(dao, customObjectTypes[0], function(objectType) {
                 self.setPageName(self.ls.get('EDIT') + ' ' + customObject.name);
                 self.ts.registerLocal('object_id', customObject._id);
                 self.ts.load('admin/content/custom_objects/edit_object',  function(err, data) {
@@ -56,12 +56,12 @@ EditObject.prototype.render = function(cb) {
                             title: self.ls.get('FIELDS')
                         }
                     ];
-                    
+
                     var fieldOrder = [];
                     for(var key in objectType.fields) {
                         fieldOrder.push(key);
                     }
-                        
+
                     var pills =
                     [
                         {
@@ -77,8 +77,8 @@ EditObject.prototype.render = function(cb) {
                             href: '/admin/content/custom_objects/new_object/' + objectType.name
                         }
                     ];
-                    
-                    result = result.split('^article_id^').join(pb.js.getAngularController(
+
+                    result = result.split('^angular_script^').join(pb.js.getAngularController(
                     {
                         navigation: pb.AdminNavigation.get(self.session, ['content', 'custom_objects'], self.ls),
                         pills: pills,
@@ -87,7 +87,7 @@ EditObject.prototype.render = function(cb) {
                         customObject: customObject,
                         fieldOrder: fieldOrder
                     }, [], 'initCustomObjectsPagination()'));
-                    
+
                     result += pb.js.getJSTag('var customObjectType = ' + JSON.stringify(objectType));
                     result += pb.js.getJSTag('var customObject = ' + JSON.stringify(customObject));
 
@@ -101,25 +101,25 @@ EditObject.prototype.render = function(cb) {
 EditObject.loadFieldOptions = function(dao, objectType, cb) {
     var self = this;
     var keys = [];
-    
+
     for(var key in objectType.fields) {
         keys.push(key);
     }
-    
+
     this.loadObjectOptions = function(index) {
         if(index >= keys.length) {
             cb(objectType);
             return;
         }
-        
+
         var key = keys[index];
-        
+
         if(objectType.fields[key].field_type != 'peer_object' && objectType.fields[key].field_type != 'child_objects') {
             index++;
             self.loadObjectOptions(index);
             return;
         }
-        
+
         if(objectType.fields[key].object_type.indexOf('custom:') > -1) {
             var customObjectTypeName = objectType.fields[key].object_type.split('custom:').join('');
             dao.query('custom_object_type', {name: customObjectTypeName}).then(function(customObjectTypes)
@@ -127,51 +127,51 @@ EditObject.loadFieldOptions = function(dao, objectType, cb) {
                 if (util.isError(customObjectTypes)) {
 		            //TODO handle this
 	            }
-	            
+
 	            if(customObjectTypes.length == 0)
 	            {
 	                return;
 	            }
-	            
+
 	            var customObjectType = customObjectTypes[0];
-            
+
                 dao.query('custom_object', {type: customObjectType._id.toString()}).then(function(customObjects) {
 		            if (util.isError(customObjects)) {
 			            //TODO handle this
 		            }
-		            
+
 		            var customObjectsInfo = [];
 		            for(var i = 0; i < customObjects.length; i++)
 		            {
 		                customObjectsInfo.push({_id: customObjects[i]._id, name: customObjects[i].name});
 		            }
-		            
+
 		            objectType.fields[key].available_objects = customObjectsInfo;
 		            index++;
 		            self.loadObjectOptions(index);
 	            });
             });
-	        
+
 	        return;
         }
-        
+
         dao.query(objectType.fields[key].object_type, pb.DAO.ANYWHERE, pb.DAO.PROJECT_ALL).then(function(availableObjects) {
 		    if (util.isError(availableObjects)) {
 			    //TODO handle this
 		    }
-		    
+
 		    var objectsInfo = [];
 		    for(var i = 0; i < availableObjects.length; i++)
 	        {
 	            objectsInfo.push({_id: availableObjects[i]._id, name: (availableObjects[i].name) ? availableObjects[i].name : availableObjects[i].headline});
 	        }
-	        
+
 	        objectType.fields[key].available_objects = objectsInfo;
 	        index++;
 	        self.loadObjectOptions(index);
 	    });
     };
-    
+
     this.loadObjectOptions(0);
 };
 
