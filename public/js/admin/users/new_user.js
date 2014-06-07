08e3ab970feb407ca91d7d55b70a66e110bb9e06
Blake Callens <blake.callens@gmail.com>
2014-06-07 11:18:26 -0400

    fixed file upload on manage account and added username check on admin user management

--- public/js/admin/users/new_user.js
+++ public/js/admin/users/new_user.js
@@ -1,7 +1,7 @@
 /*
 
     Interface for adding a new user
-    
+
     @author Blake Callens <blake.callens@gmail.com>
     @copyright PencilBlue 2013, All rights reserved
 
@@ -35,6 +35,37 @@ $(document).ready(function()
     });
 });
 
+function resetUsernameAvailability()
+{
+    $('#availability_button').attr('class', 'btn btn-default');
+    $('#availability_button').html(loc.generic.CHECK);
+}
+
+function validateUsername()
+{
+    if($('#username').val().length === 0)
+    {
+        return;
+    }
+
+    $.getJSON('/api/user/get_username_available?username=' + $('#username').val(), function(response)
+    {
+        if(response.code === 0)
+        {
+            if(response.data)
+            {
+                $('#availability_button').attr('class', 'btn btn-success');
+                $('#availability_button').html('<i class="fa fa-check"></i>&nbsp;' + loc.generic.AVAILABLE);
+            }
+            else
+            {
+                $('#availability_button').attr('class', 'btn btn-danger');
+                $('#availability_button').html('<i class="fa fa-ban"></i>&nbsp;' + loc.generic.UNAVAILABLE);
+            }
+        }
+    });
+}
+
 function checkPasswordMatch(keepType)
 {
     if(typeof keepType === 'undefined')
@@ -57,13 +88,13 @@ function checkPasswordMatch(keepType)
 function generatePassword()
 {
     var characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '?'];
-    
+
     var password = '';
     while(password.length < 8)
     {
         password = password.concat(characters[parseInt(Math.random() * characters.length)]);
     }
-    
+
     $('#password').attr('type', 'text');
     $('#password').val(password);
     $('#confirm_password').val(password);
