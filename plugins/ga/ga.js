dfa2754d1db655f602582ba18e43323021557bfe
Blake Callens <blake.callens@gmail.com>
2014-06-09 22:23:15 -0400

    finished ga plugin and fixed directive typo

--- plugins/ga/ga.js
+++ plugins/ga/ga.js
@@ -45,7 +45,9 @@ GoogleAnalytics.onStartup = function(cb) {
                     cb(null, '');
                     return;
                 }
-                cb(null, "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '" + trackingId + "', 'pencilblue.org');ga('send', 'pageview');</script>");
+                var website = pb.config.siteRoot.split('http://').join('').split('https://').join('');
+
+                cb(null, "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '" + trackingId + "', '" + website + "');ga('send', 'pageview');</script>");
             });
         });
     });
