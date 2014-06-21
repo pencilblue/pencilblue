826c3a8f4e25f95b574b4fdc595a33b172b5dd46
Blake Callens <blake.callens@gmail.com>
2014-06-21 07:58:19 -0400

    fixed bug #163

--- plugins/portfolio/controllers/index.js
+++ plugins/portfolio/controllers/index.js
@@ -93,12 +93,12 @@ Index.prototype.render = function(cb) {
                             calloutsHTML += calloutTemplate;
                         }
 
-                        content.content = content.content.split('^layout^').join(decodeURIComponent(settings.page_layout));
-                        content.content = content.content.split('^hero_image^').join(settings.home_page_hero);
+                        content.content = content.content.split('^layout^').join(decodeURIComponent(settings.page_layout ? settings.page_layout : ''));
+                        content.content = content.content.split('^hero_image^').join(settings.home_page_hero ? settings.home_page_hero : '');
                         content.content = content.content.split('^callouts^').join(calloutsHTML);
 
                         content.content = self.ls.localize([], content.content);
-                        
+
                         var angularData = pb.js.getAngularController({}, ['ngSanitize']);
                         content.content = content.content.concat(angularData);
 
@@ -111,6 +111,9 @@ Index.prototype.render = function(cb) {
 };
 
 Index.prototype.getCallouts = function(settings) {
+    if(!settings.callout_headline_1) {
+        return [];
+    }
     return [
         {
             headline: settings.callout_headline_1,
