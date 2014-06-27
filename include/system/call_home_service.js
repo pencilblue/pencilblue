9d2075d11bd33a67ed23bb949300d289bb404fc8
Brian P. Hyder <brianhyder@gmail.com>
2014-06-26 20:29:42 -0400

    Completed call home client and tested.  Will need to field test when SSL implemented for launch site

--- include/system/call_home_service.js
+++ include/system/call_home_service.js
@@ -1,14 +1,20 @@
 /**
- * @class CallHomeService
- * @constructor
  * @author Brian Hyder <brian@penciblue.org>
  * @copyright 2014 PencilBlue, LLC. All Rights Reserved
+ */
+
+//dependencies
+var os = require('os');
+
+/**
+ * @class CallHomeService
+ * @constructor
  */
 function CallHomeService(){}
 
 //constants
-var HOST   = 'pencilblue.org'
-var PORT   = 80;
+var HOST   = 'pencilblue.org';
+var PORT   = 443;
 var PATH   = '/api/v1/callhome/event';
 var METHOD = 'POST';
 
@@ -22,8 +28,13 @@ CallHomeService.callHome = function(type, data) {
     }
     
     data.type      = type;
-    data.site_ip   = pb.config.siteIp;
+    data.site_ip   = pb.config.siteIP;
     data.site_name = pb.config.siteName;
+    data.os        = os.type();
+    data.platform  = os.platform();
+    data.release   = os.release();
+    data.cpus      = os.cpus();
+    data.version   = process.versions;
     var post_data  = JSON.stringify(data);
     
     // An object of options to indicate where to post to
@@ -40,25 +51,37 @@ CallHomeService.callHome = function(type, data) {
 
     // Set up the request
     pb.log.debug('CallHomeService: Sending event [%s] to [%s:%s%s', type, METHOD, HOST, PATH);
-    var post_req = http.request(post_options, function(res) {
-        
-        var json = '';
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            json += chunk;
-        });
-        res.on('end', function() {
-            CallHomeService.onResponseRecieved(res, json);
-        });
-    });
+    CallHomeService._callHome(post_options, post_data);
+};
+
+CallHomeService._callHome = function(options, postData) {
     
-    // post the data
-    post_req.write(post_data);
-    post_req.end();
+    var d = domain.create();
+    d.on('error', function(err) {
+        pb.log.silly('CallHomeService: An error occurred attempting to send event. %s', err.stack);
+    });
+    d.run(function() {
+        
+        var post_req = https.request(options, function(res) {
+        
+            var json = '';
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                json += chunk;
+            });
+            res.on('end', function() {
+                CallHomeService.onResponseRecieved(res, json);
+            });
+        });
+
+        // post the data
+        post_req.write(postData);
+        post_req.end();
+    });
 };
 
 CallHomeService.onResponseRecieved = function(res, json) {
-    pb.log.debug('CallHomeService: Event Response: %s', json);
+    pb.log.silly('CallHomeService: Event Response: %s', json);
 };
 
 //exports
