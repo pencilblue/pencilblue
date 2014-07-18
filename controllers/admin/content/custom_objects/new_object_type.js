5e9459c2f6a076dcfacf0c2191a80176b18ffacc
Blake Callens <blake.callens@gmail.com>
2014-07-18 07:49:18 -0400

    started on refactoring angular inclusion

--- controllers/admin/content/custom_objects/new_object_type.js
+++ controllers/admin/content/custom_objects/new_object_type.js
@@ -33,46 +33,45 @@ var SUB_NAV_KEY = 'new_custom_object_type';
 NewObjectType.prototype.render = function(cb) {
 	var self = this;
 
-	this.setPageName(this.ls.get('NEW_OBJECT'));
-	this.ts.load('admin/content/custom_objects/new_object_type', function(err, data) {
-        var result = ''+data;
-        var tabs   =
-        [
-            {
-                active: 'active',
-                href: '#object_settings',
-                icon: 'cog',
-                title: self.ls.get('SETTINGS')
-            },
-            {
-                href: '#object_fields',
-                icon: 'list-ul',
-                title: self.ls.get('FIELDS')
-            }
-        ];
+    var tabs   =
+    [
+        {
+            active: 'active',
+            href: '#object_settings',
+            icon: 'cog',
+            title: self.ls.get('SETTINGS')
+        },
+        {
+            href: '#object_fields',
+            icon: 'list-ul',
+            title: self.ls.get('FIELDS')
+        }
+    ];
 
-        var objectTypes = ['article', 'page', 'section', 'topic', 'media', 'user'];
+    var objectTypes = ['article', 'page', 'section', 'topic', 'media', 'user'];
+    var dao = new pb.DAO();
+    dao.query('custom_object_type', pb.DAO.ANYWHERE, pb.DAO.PROJECT_ALL).then(function(customObjectTypes) {
+        if (util.isError(customObjectTypes)) {
+            //TODO handle this
+        }
 
-        var dao = new pb.DAO();
-        dao.query('custom_object_type', pb.DAO.ANYWHERE, pb.DAO.PROJECT_ALL).then(function(customObjectTypes) {
-		    if (util.isError(customObjectTypes)) {
-			    //TODO handle this
-		    }
+        // Case insensitive test for duplicate name
+        for(var i =0; i < customObjectTypes.length; i++) {
+            objectTypes.push('custom:' + customObjectTypes[i].name);
+        }
 
-            // Case insensitive test for duplicate name
-            for(var i =0; i < customObjectTypes.length; i++) {
-                objectTypes.push('custom:' + customObjectTypes[i].name);
-            }
+        var angularData = pb.js.getAngularController(
+        {
+            navigation: pb.AdminNavigation.get(self.session, ['content', 'custom_objects'], self.ls),
+            pills: pb.AdminSubnavService.get(SUB_NAV_KEY, self.ls, 'new_object_type'),
+            tabs: tabs,
+            objectTypes: objectTypes
+        });
 
-            var pills = pb.AdminSubnavService.get(SUB_NAV_KEY, self.ls, 'new_object_type');
-            result    = result.split('^angular_script^').join(pb.js.getAngularController(
-            {
-                navigation: pb.AdminNavigation.get(self.session, ['content', 'custom_objects'], self.ls),
-                pills: pills,
-                tabs: tabs,
-                objectTypes: objectTypes
-            }));
-
+        self.setPageName(self.ls.get('NEW_OBJECT'));
+        self.ts.registerLocal('angular_script', angularData);
+        self.ts.load('admin/content/custom_objects/new_object_type', function(err, data) {
+            var result = '' + data;
             cb({content: result});
         });
     });
