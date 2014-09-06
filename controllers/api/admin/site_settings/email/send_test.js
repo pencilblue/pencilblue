e7ee8ab6fc5752a5f45d1b64d84257310e84467a
Blake Callens <blake@pencilblue.org>
2014-09-06 08:42:37 -0400

    set up send test email call

---
+++ controllers/api/admin/site_settings/email/send_test.js
@@ -0,0 +1,70 @@
+/*
+    Copyright (C) 2014  PencilBlue, LLC
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * Resends an account verification email
+ */
+
+function SendTestEmail(){}
+
+//inheritance
+util.inherits(SendTestEmail, pb.FormController);
+
+SendTestEmail.prototype.onPostParamsRetrieved = function(post, cb) {
+    var self = this;
+
+    var message = this.hasRequiredParams(post, this.getRequiredFields());
+    if(message) {
+        self.formError(message, '/user/resend_verification', cb);
+        return;
+    }
+
+    var dao = new pb.DAO();
+    dao.loadByValue('email', post.email, 'user', function(err, user) {
+        if(util.isError(err) || user === null) {
+            self.formError(self.ls.get('USER_VERIFIED'), '/user/login', cb);
+            return;
+        }
+
+        dao.loadByValue('email', post.email, 'unverified_user', function(err, user) {
+            if(util.isError(err) || user === null) {
+                self.formError(self.ls.get('NOT_REGISTERED'), '/user/sign_up', cb);
+                return;
+            }
+
+           user.verification_code = pb.utils.uniqueId();
+
+           dao.update(user).then(function(result) {
+                if(util.isError(result)) {
+                    self.formError(self.ls.get('ERROR_SAVING'), '/user/resend_verification', cb);
+                    return;
+                }
+
+                self.session.success = self.ls.get('VERIFICATION_SENT') + user.email;
+                self.redirect('/user/verification_sent', cb);
+                pb.users.sendVerificationEmail(user, pb.utils.cb);
+            });
+        });
+    });
+};
+
+SendTestEmail.prototype.getRequiredFields = function() {
+    return ['email', 'content'];
+};
+
+//exports
+module.exports = SendTestEmail;
--- public/js/admin/site_settings/email.js
+++ public/js/admin/site_settings/email.js
@@ -59,7 +59,31 @@ function checkForCustomService()
 
 function prepareEmailSettingsSave()
 {
-    $('fieldset').append('<textarea id="verification_content" name="verification_content" style="display: none">' + $('#layout_editable').html() + '</textarea>');
+    var wysId = $('.wysiwyg').attr('id').substring('wysiwg_'.length + 1);
+    getWYSIWYGLayout(wysId, function(layout) {
+        if(!$('#verification_content').position()) {
+            $('fieldset').append('<textarea id="verification_content" name="verification_content" style="display: none">' + layout + '</textarea>');
+        }
 
-    $('#email_form').submit();
+        $('#email_form').submit();
+    });
+}
+
+function sendTestEmail() {
+    if(!$('#test_email').val().length) {
+        return;
+    }
+
+    $('#email_test_icon').removeClass('fa-send');
+    $('#email_test_icon').addClass('fa-spinner fa-spin');
+
+    var wysId = $('.wysiwyg').attr('id').substring('wysiwg_'.length + 1);
+    getWYSIWYGLayout(wysId, function(layout) {
+        $.post('/api/admin/site_settings/email/send_test', {email: $('#test_email').val(), content: layout}, function(result) {
+            $('#email_test_icon').removeClass('fa-spinner fa-spin');
+            $('#email_test_icon').addClass('fa-send');
+
+            console.log(result);
+        });
+    });
 }
