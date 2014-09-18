bb1fb1b97d3315d8f62f2f93cb97a1843b340c7e
Blake Callens <blakecallens@blakes-mbp.bcbsnc.com>
2014-09-18 14:15:29 -0400

    search through angular

--- include/client_js.js
+++ include/client_js.js
@@ -46,6 +46,15 @@ ClientJS.getAngularController = function(objects, modules, directiveJS) {
         angularController = 'var pencilblueApp = angular.module("pencilblueApp", ' + JSON.stringify(modules) + ').directive("onFinishRender", function($timeout){return {restrict: "A",link: function(scope, element, attr){if (scope.$last === true){$timeout(function(){' + directiveJS + '})}}}});';
     }
 
+    var scopeString = pb.js.getAngularObjects(objects);
+
+    angularController = angularController.concat('function PencilBlueController($scope, $sce) {' + scopeString + "};\n");
+    angularController = angularController.concat('pencilblueApp.config(["$compileProvider",function(e){e.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|javascript):/)}]);');
+
+    return pb.js.getJSTag(angularController);
+};
+
+ClientJS.getAngularObjects = function(objects) {
     var scopeString = '';
     for(var key in objects) {
         if(typeof objects[key] === 'string') {
@@ -57,11 +66,8 @@ ClientJS.getAngularController = function(objects, modules, directiveJS) {
         scopeString = scopeString.concat('$scope.' + key + ' = ' + JSON.stringify(objects[key], null, pb.log.isSilly() ? ' ' : undefined) + ";\n");
     }
 
-    angularController = angularController.concat('function PencilBlueController($scope, $sce) {' + scopeString + "};\n");
-    angularController = angularController.concat('pencilblueApp.config(["$compileProvider",function(e){e.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|javascript):/)}]);');
-
-    return pb.js.getJSTag(angularController);
-};
+    return scopeString;
+}
 
 /**
  * Creates a JS tag that loads the specified url
