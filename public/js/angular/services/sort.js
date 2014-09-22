a8551f05df64aaf25e1fdf8fde4d3ffcc9662f50
Blake Callens <blakecallens@blakes-mbp.bcbsnc.com>
2014-09-22 14:33:53 -0400

    delete article is now a REST call

--- public/js/angular/services/sort.js
+++ public/js/angular/services/sort.js
@@ -3,4 +3,35 @@ angular.module('sort', [])
 	this.sort = function(items, field, sortDesc, cb) {
 		cb($filter('orderBy')(items, field, sortDesc));
 	};
+
+	this.sortByHeader = function(items, headers, headerIndex, cb) {
+		if(headers[headerIndex].unsorted) {
+			cb(items, headers);
+			return;
+		}
+
+		var sortDesc = true;
+
+		for(var i = 0; i < headers.length; i++) {
+			if(i === headerIndex) {
+				if(headers[i].sortAsc) {
+					headers[i].sortAsc = false;
+					headers[i].sortDesc = true;
+				}
+				else {
+					headers[i].sortAsc = true;
+					headers[i].sortDesc = false;
+					sortDesc = false;
+				}
+			}
+			else {
+				headers[i].sortAsc = false;
+				headers[i].sortDesc = false;
+			}
+		}
+
+		this.sort(items, headers[headerIndex].field, sortDesc, function(items) {
+			cb(items, headers);
+		});
+	}
 })
