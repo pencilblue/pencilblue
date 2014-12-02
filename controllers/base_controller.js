1163b50e4d9bb82333890d01603396792ffc5ffb
Blake Callens <blakecallens@blakes-mbp.bcbsnc.com>
2014-12-02 16:06:59 -0500

    added plugin localization to client loc variable

--- controllers/base_controller.js
+++ controllers/base_controller.js
@@ -145,7 +145,8 @@ BaseController.prototype.requiresClientLocalization = function() {
 BaseController.prototype.requiresClientLocalizationCallback = function(flag, cb) {
 	var val = '';
 	if (this.requiresClientLocalization()) {
-		val = pb.js.includeJS(pb.UrlService.urlJoin('/localization', this.ls.language.replace('_', '-') + '.js'));
+		val = pb.js.getJSTag('var loc = ' + JSON.stringify(pb.Localization.storage[this.ls.language]) + ';');
+		//val = pb.js.includeJS(pb.UrlService.urlJoin('/localization', this.ls.language.replace('_', '-') + '.js'));
 	}
 	cb(null, new pb.TemplateValue(val, false));
 };
