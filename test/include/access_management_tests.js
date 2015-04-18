4fdc75093fb9ff68c7c84d703ce04676a9efeb0a
Brian Hyder <brianhyder@gmail.com>
2015-04-18 10:28:15 -0400

    Added a couple of unit tests for security service

---
+++ test/include/access_management_tests.js
@@ -0,0 +1,55 @@
+//depedencies
+var should        = require('should');
+var Configuration = require('../../include/config.js');
+var Lib           = require('../../lib');
+
+describe('SecurityService', function() {
+    
+    var pb = null;
+    var SecurityService = null;
+    before('Initialize the Environment with the default configuration', function() {
+        
+        pb = new Lib(Configuration.getBaseConfig());
+        SecurityService = pb.SecurityService;
+    });
+    
+    describe('SecurityService.getRoleToDisplayNameMap', function() {
+        
+        it('should provide the 5 basic roles', function() {
+            
+            var ls     = new pb.Localization('en_US');
+            var result = SecurityService.getRoleToDisplayNameMap(ls);
+            
+            result.ACCESS_ADMINISTRATOR.should.eql('ACCESS_ADMINISTRATOR');
+            result.ACCESS_MANAGING_EDITOR.should.eql('ACCESS_MANAGING_EDITOR');
+            result.ACCESS_EDITOR.should.eql('ACCESS_EDITOR');
+            result.ACCESS_WRITER.should.eql('ACCESS_WRITER');
+            result.ACCESS_USER.should.eql('ACCESS_USER');
+        });
+        
+        it('should throw when no localization instance is provided', function() {
+            SecurityService.getRoleToDisplayNameMap.bind(null).should.throwError();
+        });
+    });
+    
+    describe('SecurityService.getRoleNames', function() {
+        
+        it('should provide the 5 basic roles', function() {
+            
+            var ls     = new pb.Localization('en_US');
+            var result = SecurityService.getRoleNames(ls);
+            
+            result.should.be.instanceof(Array);
+            should(result.length === 5).be.ok;
+            should(result.indexOf('ACCESS_ADMINISTRATOR') >= 0).be.ok;
+            should(result.indexOf('ACCESS_MANAGING_EDITOR') >= 0).be.ok;
+            should(result.indexOf('ACCESS_EDITOR') >= 0).be.ok;
+            should(result.indexOf('ACCESS_WRITER') >= 0).be.ok;
+            should(result.indexOf('ACCESS_USER') >= 0).be.ok;
+        });
+        
+        it('should throw when no localization instance is provided', function() {
+            SecurityService.getRoleNames.bind(null).should.throwError();
+        });
+    });
+});
