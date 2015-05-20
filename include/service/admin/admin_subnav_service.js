d83b829f7de3a9a8b24465cd4d34017c7f13b284
btidwell <ben.tidwell@careerbuilder.com>
2015-05-20 10:27:43 -0400

    remove indexOf and replace with a null/undefined check

--- include/service/admin/admin_subnav_service.js
+++ include/service/admin/admin_subnav_service.js
@@ -122,7 +122,7 @@ module.exports = function AdminSubnavServiceModule(pb) {
                     if (items[j] && items[j].name === activePill) {
                         items[j].active = 'active';
                     }
-                    if(items[j].href.indexOf(':siteid') > -1 && data.plugin && data.plugin.site) {
+                    if(items[j].href && data.plugin && data.plugin.site) {
                         items[j].href = items[j].href.replace(':siteid', data.plugin.site);
                     }
                     navItems.push(items[j]);
