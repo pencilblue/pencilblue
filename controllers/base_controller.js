3a5a7441e89bf463a1d32e53d991fb0d9c0168dc
Brian Hyder <brianhyder@gmail.com>
2015-05-28 21:06:27 -0400

    Fix to BaseController to properly set the instance of the template service for the service context

--- controllers/base_controller.js
+++ controllers/base_controller.js
@@ -102,11 +102,6 @@ module.exports = function BaseControllerModule(pb) {
         this.pathVars            = props.path_vars;
         this.query               = props.query;
         this.pageName            = '';
-        this.context             = {
-            req: this.req,
-            session: this.session,
-            ls: this.ls
-        };
 
         var self = this;
         this.templateService     = new pb.TemplateService(this.localizationService);
@@ -132,6 +127,15 @@ module.exports = function BaseControllerModule(pb) {
             });
         });
         this.ts = this.templateService;
+        
+        //build out a base service context that can be cloned and passed to any 
+        //service objects
+        this.context = {
+            req: this.req,
+            session: this.session,
+            ls: this.ls,
+            ts: this.ts
+        };
 
         cb();
     };
