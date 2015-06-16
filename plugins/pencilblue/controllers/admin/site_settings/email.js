b670e3b08f512be7a001a2df59a9059d8e92a558
andparker <andrew.parker@careerbuilder.com>
2015-06-16 11:07:10 -0400

    Load global email settings if we do not have any site specific email settings

--- plugins/pencilblue/controllers/admin/site_settings/email.js
+++ plugins/pencilblue/controllers/admin/site_settings/email.js
@@ -52,7 +52,7 @@ module.exports = function(pb) {
             }
         ];
 
-        var emailService = new pb.EmailService(this.site, true);
+        var emailService = new pb.EmailService(this.site);
         emailService.getSettings(function(err, emailSettings) {
             var angularObjects = pb.ClientJs.getAngularObjects({
                 navigation: pb.AdminNavigation.get(self.session, ['settings', 'site_settings'], self.ls),
