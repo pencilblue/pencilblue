afccce85455efb3cea75aee2bd87ab48dd9e23bd
Blake Callens <blake@pencilblue.org>
2015-06-21 15:20:18 -0400

    manage_account page converted

--- plugins/pencilblue/controllers/user/manage_account.js
+++ plugins/pencilblue/controllers/user/manage_account.js
@@ -42,10 +42,8 @@ module.exports = function ManageAccountModule(pb) {
 
             self.setPageName(self.ls.get('MANAGE_ACCOUNT'));
             self.ts.registerLocal('image_title', self.ls.get('USER_PHOTO'));
-            self.ts.registerLocal('uploaded_image', (user.photo ? user.photo : ''));
             self.ts.registerLocal('angular_objects', new pb.TemplateValue(pb.ClientJs.getAngularObjects(objects), false));
             self.ts.load('user/manage_account', function(err, result) {
-
                 cb({content: result});
             });
         });
