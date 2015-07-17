31385217920870253dca11d978956ecb7f41c7fe
Ben Tidwell <btidwell@lat-btidwell.local>
2015-07-17 17:42:52 -0400

    use util.uniqueId() instead

--- include/service/entities/site_service.js
+++ include/service/entities/site_service.js
@@ -330,9 +330,7 @@ module.exports = function SiteServiceModule(pb) {
 
     // Generate a site unique id.
     function getUid() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random()*16|0,v=c=='x'?r:r&0x3|0x8;return v.toString(16);
-        });
+        return pb.util.uniqueId();
     }
 
     /**
