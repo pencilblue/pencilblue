3082c703708fe1d55c33742d565437bf07e8b404
iandtrapp <idt37414@uga.edu>
2015-08-03 11:09:26 -0400

    Fixed section service merge conflict

--- include/service/entities/section_service.js
+++ include/service/entities/section_service.js
@@ -33,8 +33,8 @@ module.exports = function SectionServiceModule(pb) {
     function SectionService(options) {
         this.site = pb.SiteService.getCurrentSite(options.site) || GLOBAL_SITE;
         this.onlyThisSite = options.onlyThisSite || false;
-        this.settings = pb.SettingServiceFactory.getServiceBySite(this.site, onlyThisSite);
-        this.siteQueryService = new pb.SiteQueryService({site: this.site, onlyThisSite: onlyThisSite});
+        this.settings = pb.SettingServiceFactory.getServiceBySite(this.site, this.onlyThisSite);
+        this.siteQueryService = new pb.SiteQueryService({site: this.site, onlyThisSite: this.onlyThisSite});
     }
 
     /**
