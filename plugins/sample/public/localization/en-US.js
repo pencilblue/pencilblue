7371005242993ccd7b5fe04cfa68e3eb94b89106
Brian Hyder <brianhyder@gmail.com>
2015-08-27 17:50:34 -0400

    Corrected naming of sample plugin localization file

---
+++ plugins/sample/public/localization/en-US.js
@@ -0,0 +1,4 @@
+{
+	"SAMPLE_HELLO_WORLD": "Hello World!!!",
+    "PARAMETERIZED_KEY": "This is a parameterized key.  I can pass a string value like, %s, or a number like %d.  I can even pass JSON like, %j"
+}
