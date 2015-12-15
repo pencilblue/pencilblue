5972bfa3dfdefd37b12d713ec2d47469d2d3e764
wesanderson303 <wes.anderson@careerbuilder.com>
2015-12-15 10:18:35 -0500

    Redirect uses 301 status code. Defaults to 302

--- include/http/request_handler.js
+++ include/http/request_handler.js
@@ -91,6 +91,15 @@ module.exports = function RequestHandlerModule(pb) {
     RequestHandler.staticRoutes = {};
 
     /**
+     * Redirect http codes
+     * @static
+     * @property REDIRECT_CODES
+     * @type {Object}
+     */
+    RequestHandler.REDIRECT_CODES = {MOVED_PERMANENTLY: 301,
+                                     FOUND: 302};
+
+    /**
      * The list of routes provided by the pencilblue plugin.  These routes are
      * loaded first to ensure defaults are in place before other plugins are
      * initialized.  In the future this will change so that all plugins are treated
@@ -879,7 +888,7 @@ module.exports = function RequestHandlerModule(pb) {
         var inactiveSiteAccess = route.themes[rt.site][rt.theme][rt.method].inactive_site_access;
         if (!this.siteObj.active && !inactiveSiteAccess) {
             if (this.siteObj.uid === pb.SiteService.GLOBAL_SITE) {
-                this.doRedirect('/admin');
+                this.doRedirect('/admin', RequestHandler.REDIRECT_CODES.MOVED_PERMANENTLY);
                 return;
             }
             else {
@@ -1094,7 +1103,7 @@ module.exports = function RequestHandlerModule(pb) {
         //do any necessary redirects
         var doRedirect = typeof data.redirect != "undefined";
         if(doRedirect) {
-            this.doRedirect(data.redirect);
+            this.doRedirect(data.redirect, RequestHandler.REDIRECT_CODES.MOVED_PERMANENTLY);
         }
         else {
             //output data here
@@ -1312,8 +1321,8 @@ module.exports = function RequestHandlerModule(pb) {
      * @method doRedirect
      * @param {String} location
      */
-    RequestHandler.prototype.doRedirect = function(location) {
-        this.resp.statusCode = 302;
+    RequestHandler.prototype.doRedirect = function(location, statusCode) {
+        this.resp.statusCode = statusCode || RequestHandler.REDIRECT_CODES.FOUND;
         this.resp.setHeader("Location", location);
         this.resp.end();
     };
