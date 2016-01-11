5ee905ecda4d6bdb2c9a3704a3583d8b64d5e82b
Christina Chatham <christina.chatham@careerbuilder.com>
2016-01-11 16:39:14 -0500

    tiny tweaks to help with better error handling

--- plugins/pencilblue/include/routes.js
+++ plugins/pencilblue/include/routes.js
@@ -1,19 +1,19 @@
 /*
-    Copyright (C) 2015  PencilBlue, LLC
+ Copyright (C) 2015  PencilBlue, LLC
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 //dependencies
 var path = require('path');
@@ -34,6 +34,7 @@ module.exports = function Routes(pb){
             access_level: 0,
             auth_required: false,
             setup_required: false,
+            inactive_site_access: true,
             controller: path.join(pb.config.docRoot, 'plugins', 'pencilblue', 'controllers', 'public.js'),
             content_type: 'text/html'
         },
@@ -393,6 +394,7 @@ module.exports = function Routes(pb){
         {
             method: 'post',
             path: "/actions/admin/content/navigation",
+            handler: 'post',
             access_level: pb.SecurityService.ACCESS_EDITOR,
             auth_required: true,
             inactive_site_access: true,
@@ -402,10 +404,11 @@ module.exports = function Routes(pb){
         {
             method: 'post',
             path: "/actions/admin/content/navigation/:id",
+            handler: 'put',
             access_level: pb.SecurityService.ACCESS_EDITOR,
             auth_required: true,
             inactive_site_access: true,
-            controller: path.join(pb.config.docRoot, 'plugins', 'pencilblue', 'controllers', 'actions', 'admin', 'content', 'navigation', 'edit_nav_item.js'),
+            controller: path.join(pb.config.docRoot, 'plugins', 'pencilblue', 'controllers', 'actions', 'admin', 'content', 'navigation', 'new_nav_item.js'),
             content_type: 'text/html'
         },
         {
@@ -1177,20 +1180,20 @@ module.exports = function Routes(pb){
 
         //**********************API************************
 
-      	{
+        {
             method: 'get',
             path: "/admin/elements/wysiwyg",
             access_level: pb.SecurityService.ACCESS_WRITER,
             auth_required: true,
             inactive_site_access: true,
             controller: path.join(pb.config.docRoot, 'plugins', 'pencilblue', 'controllers', 'admin', 'elements', 'wysiwyg.js'),
-      	},
+        },
         {
             handler: 'setLocale',
             path: "/user/locale",
             auth_required: false,
             controller: path.join(pb.config.docRoot, 'plugins', 'pencilblue', 'controllers', 'user', 'locale_view_controller.js'),
-      	},
+        },
 
         //articles
         {
@@ -1315,6 +1318,7 @@ module.exports = function Routes(pb){
             handler: "put",
             content_type: 'application/json',
             auth_required: true,
+            inactive_site_access: true,
             access_level: pb.SecurityService.ACCESS_EDITOR,
             controller: path.join(pb.config.docRoot, 'plugins/pencilblue/controllers/api/content/topic_api_controller.js'),
             request_body: ['application/json']
@@ -1327,6 +1331,7 @@ module.exports = function Routes(pb){
             handler: "get",
             content_type: 'application/json',
             auth_required: true,
+            inactive_site_access: true,
             access_level: pb.SecurityService.ACCESS_WRITER,
             controller: path.join(pb.config.docRoot, 'plugins/pencilblue/controllers/api/content/page_api_controller.js')
         },
@@ -1336,6 +1341,7 @@ module.exports = function Routes(pb){
             handler: "getAll",
             content_type: 'application/json',
             auth_required: true,
+            inactive_site_access: true,
             access_level: pb.SecurityService.ACCESS_WRITER,
             controller: path.join(pb.config.docRoot, 'plugins/pencilblue/controllers/api/content/page_api_controller.js')
         },
@@ -1345,6 +1351,7 @@ module.exports = function Routes(pb){
             handler: "delete",
             content_type: 'application/json',
             auth_required: true,
+            inactive_site_access: true,
             access_level: pb.SecurityService.ACCESS_EDITOR,
             controller: path.join(pb.config.docRoot, 'plugins/pencilblue/controllers/api/content/page_api_controller.js')
         },
@@ -1354,6 +1361,7 @@ module.exports = function Routes(pb){
             handler: "post",
             content_type: 'application/json',
             auth_required: true,
+            inactive_site_access: true,
             access_level: pb.SecurityService.ACCESS_WRITER,
             controller: path.join(pb.config.docRoot, 'plugins/pencilblue/controllers/api/content/page_api_controller.js'),
             request_body: ['application/json']
@@ -1364,6 +1372,7 @@ module.exports = function Routes(pb){
             handler: "put",
             content_type: 'application/json',
             auth_required: true,
+            inactive_site_access: true,
             access_level: pb.SecurityService.ACCESS_WRITER,
             controller: path.join(pb.config.docRoot, 'plugins/pencilblue/controllers/api/content/page_api_controller.js'),
             request_body: ['application/json']
