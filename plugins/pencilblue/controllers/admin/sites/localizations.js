f02c19610e453e747b60436243f0e5c38ac89040
Kyle Bechtel <kyle.bechtel@careerbuilder.com>
2016-05-10 07:33:29 -0400

    fix for localization dropdown

--- plugins/pencilblue/controllers/admin/sites/localizations.js
+++ plugins/pencilblue/controllers/admin/sites/localizations.js
@@ -1,16 +1,13 @@
 /*
  Copyright (C) 2015  PencilBlue, LLC
-
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
-
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
-
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -42,15 +39,27 @@ module.exports = function LocalizationModule(pb) {
      */
     Localization.prototype.render = function(cb) {
         var self = this;
-        setupAngularObj(self, cb);
+
+        var dao = new pb.DAO();
+        dao.loadByValue('uid', self.site, 'site', function(err, data) {
+            if (util.isError(err)) {
+                return cb(err);
+            }
+
+            if(data){
+                self.savedLocales = data.supportedLocales;
+                self.defaultLocale = data.defaultLocale;
+            }
+            setupAngularObj(self, cb);
+        });
     };
 
     function setupAngularObj(self, cb){
         var pluginService = new pb.PluginService({site: self.site});
         var activePlugins = pluginService.getActivePluginNames();
-        var savedLocales = {},
+        var savedLocales = self.savedLocales || {},
             selectedLocales,
-            defaultLocale = pb.Localization.getDefaultLocale();
+            defaultLocale = self.defaultLocale || pb.Localization.getDefaultLocale();
 
         self.ts.registerLocal("active_theme", new pb.TemplateValue(self.activeTheme,false));
         savedLocales[defaultLocale] = true;
