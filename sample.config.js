fa657cc2b3807a373880e737f08329dd0e375cbc
Marcos Rava <marcos.rava@flytour.com.br>
2016-12-29 16:05:55 -0200

    Refactor sample.config.js - ECMA6, airbnb linter

--- sample.config.js
+++ sample.config.js
@@ -10,56 +10,56 @@
  */
 
 module.exports = {
-    "siteName": "My PencilBlue Site",
-    "siteRoot": "http://localhost:8080",
-    "sitePort": 8080,
-    "logging": {
-        "level": "info"
+  siteName: 'My PencilBlue Site',
+  siteRoot: 'http://localhost:8080',
+  sitePort: 8080,
+  logging: {
+    level: 'info',
+  },
+  db: {
+    type: 'mongo',
+    servers: [
+      '127.0.0.1:27017',
+    ],
+    name: 'pencilblue',
+    writeConcern: 1,
+    query_logging: false,
+  },
+  cache: {
+    fake: true,
+    host: 'localhost',
+    port: 6379,
+  },
+  settings: {
+    use_memory: false,
+    use_cache: false,
+  },
+  templates: {
+    use_memory: true,
+    use_cache: false,
+  },
+  plugins: {
+    caching: {
+      use_memory: false,
+      use_cache: false,
     },
-    "db": {
-        "type":"mongo",
-        "servers": [
-          "127.0.0.1:27017"
-        ],
-        "name": "pencilblue",
-        "writeConcern": 1,
-        query_logging: false
-    },
-    "cache": {
-        "fake": true,
-        "host": "localhost",
-        "port": 6379
-    },
-    "settings": {
-        "use_memory": false,
-        "use_cache": false
-    },
-    "templates": {
-        "use_memory": true,
-        "use_cache": false
-    },
-    "plugins": {
-        "caching": {
-            "use_memory": false,
-            "use_cache": false
-        }
-    },
-    "registry": {
-        "type": "redis"
-    },
-    "session": {
-        "storage": "mongo"
-    },
-    "media": {
-        "provider": "mongo",
-        "max_upload_size": 6 * 1024 * 1024
-    },
-    "cluster": {
-        "workers": 1,
-        "self_managed": false
-    },
-    multisite: {
-        enabled: false,
-        globalRoot: 'http://global.localhost:8080'
-    }
+  },
+  registry: {
+    type: 'redis',
+  },
+  session: {
+    storage: 'mongo',
+  },
+  media: {
+    provider: 'mongo',
+    max_upload_size: 6 * 1024 * 1024,
+  },
+  cluster: {
+    workers: 1,
+    self_managed: false,
+  },
+  multisite: {
+    enabled: false,
+    globalRoot: 'http://global.localhost:8080',
+  },
 };
