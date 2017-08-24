e5d2d58034865a75503c01cc5a7a85ea7514acdb
seanpaw <sean.casey@careerbuilder.com>
2017-08-24 09:38:38 -0400

    Update config.js

--- include/config.js
+++ include/config.js
@@ -153,7 +153,7 @@ Configuration.getBaseConfig = function(multisite) {
             name: 'pencil_blue',
 
             options: {
-                connectWithNoPrimary: true
+                connectWithNoPrimary: true,
                 w: 1,
                 replicaSet: 'replset-1',
                 readPreference: 'secondaryPreferred'
