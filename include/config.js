cdf2464bf818156aecebed62728a01ae4b614848
seanpaw <sean.casey@careerbuilder.com>
2017-09-21 15:16:21 -0400

    Revert "Update Write Preference"

--- include/config.js
+++ include/config.js
@@ -153,7 +153,7 @@ Configuration.getBaseConfig = function(multisite) {
             name: 'pencil_blue',
 
             options: {
-                w: 3,
+                w: 1,
                 replicaSet: 'replset-1',
                 readPreference: 'secondaryPreferred'
             },
