53429859ca0c641722dd14a285e3113db717ca0d
btidwell <ben.tidwell@careerbuilder.com>
2017-11-09 08:41:09 -0500

    revert back to wrapTask instead of timedTask

--- pencilblue.js
+++ pencilblue.js
@@ -67,21 +67,21 @@ function PencilBlue(config){
      */
     this.init = function(){
         var tasks = [
-            util.wrapTimedTask(this, this.initModules, 'initModules'),
-            util.wrapTimedTask(this, this.initRequestHandler, 'initRequestHandler'),
-            util.wrapTimedTask(this, this.initDBConnections, 'initDBConnections'),
-            util.wrapTimedTask(this, this.initDBIndices, 'initDBIndices'),
-            util.wrapTimedTask(this, this.initServerRegistration, 'initServerRegistration'),
-            util.wrapTimedTask(this, this.initCommandService, 'initCommandService'),
-            //util.wrapTimedTask(this, this.initSiteMigration, 'initSiteMigration'),
-            util.wrapTimedTask(this, this.initSessions, 'initSessions'),
-            util.wrapTimedTask(this, this.initMiddleware, 'initMiddleware'),
-            util.wrapTimedTask(this, this.initPlugins, 'initPlugins'),
-            util.wrapTimedTask(this, this.initSites, 'initSites'),
-            util.wrapTimedTask(this, this.initLocales, 'initLocales'),
-            util.wrapTimedTask(this, this.initLibraries, 'initLibraries'),
-            util.wrapTimedTask(this, this.registerMetrics, 'registerMetrics'),
-            util.wrapTimedTask(this, this.initServer, 'initServer')
+            util.wrapTask(this, this.initModules, 'initModules'),
+            util.wrapTask(this, this.initRequestHandler, 'initRequestHandler'),
+            util.wrapTask(this, this.initDBConnections, 'initDBConnections'),
+            util.wrapTask(this, this.initDBIndices, 'initDBIndices'),
+            util.wrapTask(this, this.initServerRegistration, 'initServerRegistration'),
+            util.wrapTask(this, this.initCommandService, 'initCommandService'),
+            //util.wrapTask(this, this.initSiteMigration, 'initSiteMigration'),
+            util.wrapTask(this, this.initSessions, 'initSessions'),
+            util.wrapTask(this, this.initMiddleware, 'initMiddleware'),
+            util.wrapTask(this, this.initPlugins, 'initPlugins'),
+            util.wrapTask(this, this.initSites, 'initSites'),
+            util.wrapTask(this, this.initLocales, 'initLocales'),
+            util.wrapTask(this, this.initLibraries, 'initLibraries'),
+            util.wrapTask(this, this.registerMetrics, 'registerMetrics'),
+            util.wrapTask(this, this.initServer, 'initServer')
         ];
         async.series(tasks, function(err, results) {
             if (util.isError(err)) {
