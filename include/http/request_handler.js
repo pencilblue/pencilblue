b55e2f4fc250125c5c7338733b0c8f5821e14a9c
Logan Genet <logan.genet@careerbuilder.com>
2018-09-17 21:25:07 -0400

    Adding in a module to handle cookie parsing.  Also attached it to base controller for easy access to it.

--- include/http/request_handler.js
+++ include/http/request_handler.js
@@ -677,6 +677,7 @@ module.exports = function RequestHandlerModule(pb) {
                 request: ctx.req,
                 response: ctx.res,
                 session: ctx.session,
+                cookies: ctx.cookies,
                 localization_service: ctx.req.localizationService,
                 path_vars: ctx.params, // TODO: Remove this one
                 pathVars: ctx.params,
