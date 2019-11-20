c6fef2b32910a66593bbc2b822748f0ea0aca67b
Enmanuel <enmachs@gmail.com>
2019-11-20 17:44:42 -0400

    [ACEAO-5906] Add security patch - avoid admin updating process

--- plugins/pencilblue/controllers/actions/user/manage_account/profile.js
+++ plugins/pencilblue/controllers/actions/user/manage_account/profile.js
@@ -61,9 +61,15 @@ module.exports = function UserProfileApiControllerModule(pb) {
      * @param {Function} cb
      */
     UserProfileApiController.prototype.put = function(cb) {
+        var self = this;
         var dto = this.body || {};
-        dto[pb.DAO.getIdField()] = this.session.authentication.user_id;
-        this.service.save(dto, this.handleSave(cb, false));
+        var sessionUserId = this.session.authentication.user_id;
+
+        this.service.getById(sessionUserId, function(err, value) {
+            dto.admin = value.admin;
+            dto[pb.DAO.getIdField()] = sessionUserId;
+            self.service.save(dto, self.handleSave(cb, false));
+        });
     };
 
     //exports
